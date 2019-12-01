f23520457f5f1db7ae379f5f70af0a4161aa92a7
panda <pxzlin@qq.com>
2019-12-01 23:32:44 +0800

    u

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -41,35 +41,60 @@ import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
-import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+// import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+
+import deleteIcon from '../static/theme/icons/delete.svg';
 
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 
-class InsertImage extends Plugin {
+// class InsertImage extends Plugin {
+// 	init() {
+// 		const editor = this.editor;
+
+// 		editor.ui.componentFactory.add( 'insertImage', locale => {
+// 			const view = new ButtonView( locale );
+
+// 			view.set( {
+// 				label: 'Insert image',
+// 				icon: imageIcon,
+// 				tooltip: true
+// 			} );
+
+// 			// Callback executed once the image is clicked.
+// 			view.on( 'execute', () => {
+// 				const imageUrl = prompt( 'Image URL' );
+
+// 				editor.model.change( writer => {
+// 					const imageElement = writer.createElement( 'image', {
+// 						src: imageUrl
+// 					} );
+
+// 					// Insert the image in the current selection location.
+// 					editor.model.insertContent( imageElement, editor.model.document.selection );
+// 				} );
+// 			} );
+
+// 			return view;
+// 		} );
+// 	}
+// }
+
+class ClearContent extends Plugin {
 	init() {
 		const editor = this.editor;
 
-		editor.ui.componentFactory.add( 'insertImage', locale => {
+		editor.ui.componentFactory.add( 'clearContent', locale => {
 			const view = new ButtonView( locale );
 
 			view.set( {
-				label: 'Insert image',
-				icon: imageIcon,
+				label: 'Clear content',
+				icon: deleteIcon,
 				tooltip: true
 			} );
 
 			// Callback executed once the image is clicked.
 			view.on( 'execute', () => {
-				const imageUrl = prompt( 'Image URL' );
-
-				editor.model.change( writer => {
-					const imageElement = writer.createElement( 'image', {
-						src: imageUrl
-					} );
-
-					// Insert the image in the current selection location.
-					editor.model.insertContent( imageElement, editor.model.document.selection );
-				} );
+				editor.setData( '' );
 			} );
 
 			return view;
@@ -77,14 +102,14 @@ class InsertImage extends Plugin {
 	}
 }
 
-
 // import '../theme/black.css';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	InsertImage,
+	// InsertImage,
+	ClearContent,
 	Essentials,
 	UploadAdapter,
 	Autoformat,
@@ -171,7 +196,8 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'clearContent'
 		]
 	},
 	image: {
